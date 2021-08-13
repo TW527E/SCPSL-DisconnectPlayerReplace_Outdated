00cd51a4f14622c9d2b0a28fda54c94fd004432d
TW527E <37478187+TW527E@users.noreply.github.com>
2021-08-13 18:36:50 +0800

    Improved the code

--- SCPReplace/Plugin.cs
+++ SCPReplace/Plugin.cs
@@ -5,25 +5,27 @@ namespace SCPReplace
 {
 	public class SCPReplace : Plugin<Configs>
 	{
-		public static SCPReplace SCPReplaceRef { get; set; }
+		public static SCPReplace SCPReplaceRef;
+		public override string Prefix => "SCPReplace";
 
-		public EventHandler eventHandlers;
-
-		public SCPReplace()
-		{
-			SCPReplaceRef = this;
-		}
+		private EventHandler eventHandlers;
 
 		public override void OnEnabled()
 		{
+			SCPReplaceRef = this;
 			eventHandlers = new EventHandler();
+
 			Player.Left += eventHandlers.OnPlayerLeft;
+
+			base.OnEnabled();
 		}
 
 		public override void OnDisabled()
 		{
 			Player.Left -= eventHandlers.OnPlayerLeft;
 			eventHandlers = null;
+			SCPReplaceRef = null;
+			base.OnDisabled();
 		}
 	}
 }
