f5488b436589b2edd18343982bfe599b0d91a0e9
TW527E <37478187+TW527E@users.noreply.github.com>
2021-08-22 15:51:56 +0800

    Fix Somethings
    Fix Somethings

--- DisconnectPlayerReplace/EventHandler.cs
+++ DisconnectPlayerReplace/EventHandler.cs
@@ -11,22 +11,20 @@ namespace DisconnectPlayerReplace
     {
         public void OnPlayerLeft(LeftEventArgs ev)
         {
-            var savedPos = ev.Player.Position;
-            var savedHealth = ev.Player.Health;
             if (DisconnectPlayerReplace.DisconnectPlayerReplaceRef.Config.OnlyScp == false)
             {
-                ChangePlayer(ev.Player, savedHealth, savedPos);
+                ChangePlayer(ev.Player);
             }
             else
             {
                 if (ev.Player.Team == Team.SCP)
                 {
-                    ChangePlayer(ev.Player, savedHealth, savedPos);
+                    ChangePlayer(ev.Player);
                 }
             }
         }
 
-        public void ChangePlayer(Player pLayer, float savedHealth, UnityEngine.Vector3 savedPos)
+        public void ChangePlayer(Player pLayer)
         {
             var RIPPlayer = Player.Get(Team.RIP);
             int count = RIPPlayer.Count();
@@ -36,8 +34,8 @@ namespace DisconnectPlayerReplace
                 var spectators = RIPPlayer.ToList();
                 var player = spectators[cplayer];
                 player.SetRole(pLayer.Role, SpawnReason.Respawn, false);
-                player.Health = savedHealth;
-                Timing.CallDelayed(0.25f, () => player.Position = savedPos);
+                player.Health = pLayer.Health;
+                Timing.CallDelayed(1f, () => player.Position = pLayer.Position);
                 Map.Broadcast(duration: 15, message: $"{ DisconnectPlayerReplace.DisconnectPlayerReplaceRef.Config.OnPlayerReplace.Replace("%Role%", pLayer.Role.ToString()).Replace("Scp", "SCP - ")}");
                 Cassie.Message(message: $"{ DisconnectPlayerReplace.DisconnectPlayerReplaceRef.Config.OnPlayerReplace.Replace("%Role%", pLayer.Role.ToString())}", false, DisconnectPlayerReplace.DisconnectPlayerReplaceRef.Config.IsNoisy);
             }
